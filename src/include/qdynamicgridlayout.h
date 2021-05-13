706af45289f803c65157d74e88edba1133709299
timurchak <timurchak@gmail.com>
2021-05-13 18:57:04 +0300

    Start future branch
    Release auto calc position with Width and Height

--- src/include/qdynamicgridlayout.h
+++ src/include/qdynamicgridlayout.h
@@ -3,19 +3,40 @@
 
 #include <QObject>
 #include <QGridLayout>
+#include <QMap>
+#include <QWidget>
+#include <QDebug>
 
 class QDynamicGridLayout : public QGridLayout
-{
+{   
     Q_OBJECT
 public:
-    QDynamicGridLayout(int maxColumn = 4, QWidget* parent = nullptr);
+    enum class ModeGrid {FixedColumn = 0, FixedWidth, FixedHeight};
+    QDynamicGridLayout(ModeGrid _mode = ModeGrid::FixedColumn, QWidget* parent = nullptr);
     bool add(QWidget* w, int r = -1, int c = -1);
     bool add(QLayoutItem* item, int r = -1, int c = -1);
     bool add(QLayout* lay, int r = -1, int c = -1);
     void setColumn(int column);
+    void setWidth(int width);
+    void setHeight(int height);
 private:
     void editIndexes();
-private:
+    void editWidth();
+    void editHeight();
+    void editPos();
+    struct DynamicItem {
+        QWidget* w = nullptr;
+        QLayoutItem* li = nullptr;
+        QLayout* l = nullptr;
+        QSize hint = QSize(0,0);
+        int r = -1;
+        int c = -1;;
+    };
+private:        
+    ModeGrid mode;
+    QMap<int, QMap<int, DynamicItem>> grid;
+    int maxWidth;
+    int maxHeight;
     int maxColumn;
     int currentRow;
     int currentColumn;
--- src/qdynamicgridlayout.cpp
+++ src/qdynamicgridlayout.cpp
@@ -1,8 +1,10 @@
 #include "qdynamicgridlayout.h"
 
-QDynamicGridLayout::QDynamicGridLayout(int maxColumn, QWidget* parent)
+QDynamicGridLayout::QDynamicGridLayout(ModeGrid _mode, QWidget* parent)
     : QGridLayout(parent)
-    , maxColumn(maxColumn)
+    , mode(_mode)
+    , grid()
+    , maxColumn(0)
     , currentRow(0)
     , currentColumn(0)
 {
@@ -11,14 +13,23 @@ QDynamicGridLayout::QDynamicGridLayout(int maxColumn, QWidget* parent)
 bool QDynamicGridLayout::add(QWidget* w, int r, int c)
 {
     if (r < 0 || c < 0) {
+        editPos();
+        grid[currentRow][currentColumn].w = w;
+        grid[currentRow][currentColumn].hint = w->sizeHint();
         addWidget(w, currentRow, currentColumn);
-        editIndexes();
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
+        //editPos();
         return true;
     } else {
         if (itemAtPosition(r, c) == nullptr) {
             return false;
         }
-        addWidget(w, r, c);
+        addWidget(w, r, c);        
+        grid[currentRow][currentColumn].w = w;
+        grid[currentRow][currentColumn].hint = w->sizeHint();
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
         return true;
     }
 }
@@ -26,14 +37,25 @@ bool QDynamicGridLayout::add(QWidget* w, int r, int c)
 bool QDynamicGridLayout::add(QLayoutItem* item, int r, int c)
 {
     if (r < 0 || c < 0) {
+        editPos();
         addItem(item, currentRow, currentColumn);
-        editIndexes();
+
+        grid[currentRow][currentColumn].li = item;
+        grid[currentRow][currentColumn].hint = item->sizeHint();
+        addItem(item, currentRow, currentColumn);
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
+        //editPos();
         return true;
     } else {
         if (itemAtPosition(r, c) == nullptr) {
             return false;
         }
-        addItem(item, r, c);
+        addItem(item, r, c);        
+        grid[currentRow][currentColumn].li = item;
+        grid[currentRow][currentColumn].hint = item->sizeHint();
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
         return true;
     }
 }
@@ -41,30 +63,83 @@ bool QDynamicGridLayout::add(QLayoutItem* item, int r, int c)
 bool QDynamicGridLayout::add(QLayout* lay, int r, int c)
 {
     if (r < 0 || c < 0) {
+        editPos();
+        addLayout(lay, currentRow, currentColumn);        
+        grid[currentRow][currentColumn].l = lay;
+        grid[currentRow][currentColumn].hint = lay->sizeHint();
         addLayout(lay, currentRow, currentColumn);
-        editIndexes();
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
         return true;
     } else {
         if (itemAtPosition(r, c) == nullptr) {
             return false;
         }
-        addLayout(lay, r, c);
+        addLayout(lay, r, c);        
+        grid[currentRow][currentColumn].l = lay;
+        grid[currentRow][currentColumn].hint = lay->sizeHint();
+        grid[currentRow][currentColumn].c = currentColumn;
+        grid[currentRow][currentColumn].r = currentRow;
         return true;
     }
 }
 
-void QDynamicGridLayout::setColumn(int column) {
+void QDynamicGridLayout::setColumn(int column)
+{
+    mode = ModeGrid::FixedColumn;
     maxColumn = column;
     int i = 0;
+    grid.clear();
     QList<QLayoutItem*> list;
-    while(itemAt(i) != nullptr) {
+    while (itemAt(i) != nullptr) {
         list.push_back(itemAt(i));
         i++;
     }
     currentRow = 0;
     currentColumn = 0;
     auto it = list.begin();
-    while(it != list.end()) {
+    while (it != list.end()) {
+        add(*it);
+        ++it;
+    }
+}
+
+void QDynamicGridLayout::setWidth(int width)
+{
+    mode = ModeGrid::FixedWidth;
+    maxWidth = width;
+    int i = 0;
+    grid.clear();
+    QList<QLayoutItem*> list;
+    while (itemAt(i) != nullptr) {
+        list.push_back(itemAt(i));
+        i++;
+    }
+    currentRow = 0;
+    currentColumn = 0;
+    auto it = list.begin();
+    while (it != list.end()) {
+        add(*it);
+        ++it;
+    }
+}
+
+void QDynamicGridLayout::setHeight(int height)
+{
+    mode = ModeGrid::FixedHeight;
+    maxHeight = height;
+    int i = 0;
+    grid.clear();
+    QList<QLayoutItem*> list;
+    while (itemAt(i) != nullptr) {
+        list.push_back(itemAt(i));
+        i++;
+    }
+
+    currentRow = 0;
+    currentColumn = 0;
+    auto it = list.begin();
+    while (it != list.end()) {
         add(*it);
         ++it;
     }
@@ -72,9 +147,52 @@ void QDynamicGridLayout::setColumn(int column) {
 
 void QDynamicGridLayout::editIndexes()
 {
-    currentColumn++;
+    // currentColumn++;
     if (currentColumn == maxColumn) {
         currentRow++;
         currentColumn = 0;
     }
+}
+
+void QDynamicGridLayout::editWidth()
+{
+    int sum = 0;
+    for (auto& item : grid[currentRow]) {
+        sum += item.hint.width();
+    }
+    if (sum > maxWidth) {
+        currentRow++;
+        currentColumn = 0;
+    }
+    qDebug() << "sum = " << sum << "maxW" << maxWidth;
+    qDebug() << "cr = " << currentRow << "cc = " << currentColumn;
+    qDebug() << "sizeRow = " << grid.size();
+}
+
+void QDynamicGridLayout::editHeight(){
+
+    int sum = 0;
+    for (auto& item : grid[currentRow]) {
+        sum += item.hint.height();
+    }
+    if (sum > maxHeight) {
+        currentRow++;
+        currentColumn = 0;
+    }
+}
+
+void QDynamicGridLayout::editPos()
+{
+    switch (mode) {
+    case ModeGrid::FixedColumn:
+        editIndexes();
+        break;
+    case ModeGrid::FixedHeight:
+        editHeight();
+        break;
+    case ModeGrid::FixedWidth:
+        editWidth();
+        break;
+    }
+    currentColumn++;
 }
